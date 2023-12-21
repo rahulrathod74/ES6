b0a583549bb962971d415cd78677522f8fc0b043
Rahul Rameshwar Rathod <rahulrathod741258@gmail.com>
2023-12-21 13:09:06 +0530

    Initial commit

---
+++ counter/script.js
@@ -0,0 +1,30 @@
+const counter=document.querySelector("#counter")
+const btns=document.querySelectorAll(".btn")
+
+console.log(btns)
+let count=0
+btns.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        const styles=e.currentTarget.classList
+
+        if(styles.contains("increase")){
+            count++; 
+        }else if(styles.contains("decrease")){
+            count--;
+        }
+        if(styles.contains("reset")){
+            count=0
+        }
+        if(count>0){
+           counter.style.color="green" 
+        }
+        if(count<0){
+            counter.style.color="red"
+        }
+
+        if(count==0){
+            counter.style.color="orange"
+        }
+        counter.textContent=count
+    })
+})
